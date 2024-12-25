6c80ffc416a9436c84bb1c9e422e12137ea1e041
Devanshu Kumar <86472136+devanshu3186@users.noreply.github.com>
2024-12-25 09:34:41 +0530

    Add files via upload

---
+++ Day 1/first_program.py
@@ -0,0 +1,34 @@
+print("Hello World!, Welcome to Python Programming.")
+print("By enclosing the text in double quotes, we can print the text in Python.")
+print("if you want to print the text in single quotes, you can use single quotes.")
+print("If you want to printf multiple lines, you can use triple double quotes or triple single quotes.\n") # \n is used to print a new line.
+
+# Write a program to print a poem in Python. Choose the poem of your choice and publish your reply in the comment section.
+
+print("""Twinkle, twinkle, little star,
+How I wonder what you are!
+Up above the world so high,
+Like a diamond in the sky.
+Twinkle, twinkle, little star,
+How I wonder what you are
+When the blazing sun is gone,
+When he nothing shines upon,
+Then you show your little light,
+Twinkle, twinkle, all the night.
+Twinkle, twinkle, little star,
+How I wonder what you are!\n""")  # \n is used to print a new line.
+# Output: Twinkle, twinkle, little star poem will be printed on the console.
+
+print('''Twinkle, twinkle, little star,
+How I wonder what you are!
+Up above the world so high,
+Like a diamond in the sky.
+Twinkle, twinkle, little star,
+How I wonder what you are
+When the blazing sun is gone,
+When he nothing shines upon,
+Then you show your little light,
+Twinkle, twinkle, all the night.
+Twinkle, twinkle, little star,
+How I wonder what you are!''')
+# here we are using triple single quotes to print the poem.
